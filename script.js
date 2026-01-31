226a633cbd1675f4a41337b4def3097657e70ed5
Chenix_208 <95759389+cheniya299@users.noreply.github.com>
2026-01-31 13:59:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ function validateConfig() {
     const warnings = [];
 
     // Check required fields
-    if (!config.valentineName) {
-        warnings.push("Valentine's name is not set! Using default.");
-        config.valentineName = "My Babyy Girlll!";
+
     }
 
     // Validate colors
