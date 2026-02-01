a03ed7cfab7744081bd6732c3f316384ac0e07a6
Chenix_208 <95759389+cheniya299@users.noreply.github.com>
2026-02-01 23:13:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "Would you come with me for a online date, madam?!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
